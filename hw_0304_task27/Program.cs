ae7146b550a7bc713344fa6607a42660fff9a75b
Sergei.Petrushin <sjabrail@gmail.com>
2023-05-24 20:55:32 +0530

    program is done

---
+++ hw_0304_task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNumber()
+{
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int DigitSum(int num)
+{
+    int result = 0,
+    temp = 0;
+    while(num > 0)
+    {
+        temp = num % 10;
+        result += temp;
+        num /= 10;
+    }
+    return result;
+}
+int numder = InputNumber();
+int result = DigitSum(numder);
+Console.WriteLine(result);
