1548135a0fbec4205226f202f1a9a32f17510b28
Sergei.Petrushin <sjabrail@gmail.com>
2023-05-24 16:51:01 +0530

    program is done

---
+++ hw_0304_task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите длину массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+void RandomArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[a];
+RandomArr(array);
+PrintArray(array);
